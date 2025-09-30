b12d33526a7d8f0772bcc4fcca0c936430b7d7f7
Sasidhar Veerni <sasidharveerni2@gmail.com>
2025-09-30 23:53:39 +0530

    feat: add payment links to event registration options in EventDetailsPage

--- mindful-frontend/src/components/EventDetailsPage.jsx
+++ mindful-frontend/src/components/EventDetailsPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import EventBanner from "./EventBanner";
@@ -113,31 +113,37 @@ export default function EventDetailsPage({ mockEvents }) {
                   {
         title: "Speaker Registration",
         price: "$599",
+        paymentLink: 'https://buy.stripe.com/9B63cv16OeCe0WZ1Nh8k807',
         features: ["Certificate", "CPD Credits", "Breakfast & Lunch", "Speaker Slot"],
       },
       {
         title: "Package-A",
         price: "$899",
+        paymentLink: 'https://buy.stripe.com/6oUfZhaHo51E9tv9fJ8k803',
         features: ["Certificate", "CPD Credits", "Breakfast & Lunch", "Speaker Slot + 2 Nights Accommodation"],
       },
       {
         title: "Package-B",
         price: "$1,099",
+        paymentLink: 'https://buy.stripe.com/5kQ5kD9Dk9hU213gIb8k804',
         features: ["Certificate", "CPD Credits", "Breakfast & Lunch", "Speaker Slot + 3 Nights Accommodation"],
       },
       {
         title: "Exhibitor Pass",
         price: "$1,399",
+        paymentLink: 'https://buy.stripe.com/cNi8wP02KeCeaxz2Rl8k805',
         features: ["Certificate", "CPD Credits", "Exhibitor Booth", "Speaker Slot"],
       },
       {
         title: "Delegate Pass",
         price: "$299",
+        paymentLink: 'https://buy.stripe.com/fZu7sLdTAfGibBD4Zt8k806',
         features: ["Certificate", "CPD Credits", "Breakfast & Lunch"],
       },
       {
         title: "Virtual Pass",
         price: "$299",
+        paymentLink: 'https://buy.stripe.com/fZu7sLdTAfGibBD4Zt8k806',
         features: ["Certificate", "CPD Credits",],
       },
                 ].map((plan, index) => (
@@ -167,12 +173,14 @@ export default function EventDetailsPage({ mockEvents }) {
                     {/* Price & Button */}
                     <div className="flex flex-col items-start mt-6">
                       <p className="text-2xl font-bold text-[#1c160d]">{plan.price}</p>
-                      <button className="mt-4 bg-[#f29e0d] hover:bg-[#e68c00] text-[#1c160d] px-5 py-2 rounded-lg font-semibold text-sm flex items-center gap-2 transition-colors">
-                        Register Now 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 256">
-                          <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"/>
-                        </svg>
-                      </button>
+                      <Link to={plan.paymentLink} target="_blank">
+                          <button className="mt-4 bg-[#f29e0d] hover:bg-[#e68c00] text-[#1c160d] px-5 py-2 rounded-lg font-semibold text-sm flex items-center gap-2 transition-colors">
+                            Register Now 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 256 256">
+                              <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"/>
+                            </svg>
+                          </button>
+                      </Link>
                     </div>
                   </div>
                 ))}
